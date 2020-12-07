98705db55dbcaaf3f4441b88d0cec4d32caaadf9
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-12-07 04:52:43 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,11 +1,12 @@
 # Code your solution h
-
+require "pry"
 def run_guessing_game
   puts "Guess a number between 1 and 6."
   input = gets.chomp
   number = rand(6) + 1
 
   while input != "exit" do 
+ binding.pry
     if input.to_i == number
       puts "You guessed the correct number!"
     else
